6baaa577a08bf5e43873d22736220f647b3b16a6
ChatGIS <ChatGIS@outlook.com>
2024-10-20 22:49:25 +0800

    update Python0301

--- Python03面向对象/Python0301.py
+++ Python03面向对象/Python0301.py
@@ -1,26 +1,62 @@
 '''
 函数定义
 '''
-def myPrint(content):
+def print_content(content):
     print('ChatGIS:', content)
 
-myPrint('Number One')
+print_content('Number One')
 
 '''
 默认参数
 '''
-def myPrintDefaultParam(name='ChatGIS'):
+def print_default_param(name='ChatGIS'):
     print(name, 'Number One')
-myPrintDefaultParam()
-myPrintDefaultParam('China')
+print_default_param()
+print_default_param('China')
 
 '''
 多参数
 '''
-def calculateArea(width, height):
+def calculate_area(width, height):
     print(width, height)
     return width * height
-print(calculateArea(2, 3))
-print(calculateArea(height=4, width=5))
-print(calculateArea(4, height=5))
-# print(calculateArea(width=4, 5))
+print(calculate_area(2, 3))
+print(calculate_area(height=4, width=5))
+print(calculate_area(4, height=5))
+# print(calculate_area(width=4, 5))
+
+'''
+变量作用域
+'''
+name = 'gChatGIS'
+def show_name():
+    name = 'ChatGIS'
+    print('in:', name)
+show_name()
+print('out: ', name)
+
+'''
+匿名函数
+'''
+# 常规函数
+def double(x):
+    return x * 2
+print(double(2))
+# 匿名函数
+doubx = lambda x: x * 2
+print(doubx(2))
+
+'''
+生成器
+'''
+def my_generator():
+    yield 1
+    yield 2
+    yield 3
+print(next(my_generator()))
+print(next(my_generator()))
+print(next(my_generator()))
+gen = my_generator()
+print(next(gen))
+print(next(gen))
+print(next(gen))
