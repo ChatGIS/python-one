d4eb45aa196b1a5e20dbd6d5b5ba54f5b1f2098f
ChatGIS <ChatGIS@outlook.com>
2025-01-10 00:18:46 +0800

    v1.0.0 init vitepress

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,77 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  base: '/python-one/',
+  title: "Python One",
+  head: [['link', { rel: 'icon', href: 'https://chatgis.space/images/favicon.png' }]],
+  description: "One notebook of Python",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '主页', link: '/' },
+      { text: 'Python入门', link: '/python/0101' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Python入门教程',
+        items: [
+          { text: '你好, Python', link: '/python/0101' },
+          {
+            text: 'Python基础', 
+            collapsed: false,
+            items: [{
+              text: '注释、标识符、关键字', link: '/python/0201'
+            }, {
+              text: '变量', link: '/python/0202'
+            }, {
+              text: '代码缩进', link: '/python/0203'
+            }, {
+              text: '基本数据类型', link: '/python/0204'
+            }, {
+              text: '高级数据类型', link: '/python/0205'
+            }, {
+              text: '运算符', link: '/python/0206'
+            }, {
+              text: '流程控制', link: '/python/0207'
+            }] 
+          },
+          {
+            text: '面向对象',
+            collapsed: false,
+            items: [
+              {
+                text: '函数', link: '/python/0301'
+              },{
+                text: '面向对象', link: '/python/0302'
+              }
+            ]
+          },
+          {
+            text: '文件',
+            collapsed: false,
+            items: [
+              {
+                text: '文件', link: '/python/0401'
+              }
+            ]
+          },
+          {
+            text: 'GUI',
+            collapsed: false,
+            items: [
+              {
+                text: '选型', link: '/python/0501'
+              }
+            ]
+          }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/ChatGIS/python-one' }
+    ]
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,7 @@
+---
+layout: doc
+---
+## Python One
+Python One (Python集、Python一本书)，用于个人整理Python相关的学习内容，包括文章以及源码。
+
+[关于作者](https://chatgis.space/about/)
