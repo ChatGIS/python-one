ab63eba5f194154834241ea5f90e3c29eb7907cf
ChatGIS <ChatGIS@outlook.com>
2025-04-27 00:59:49 +0800

    v1.4.3 update oop

--- codes/03/01.py
+++ codes/03/01.py
@@ -1,33 +1,42 @@
 class Student:
     school = '集思小学'
     student_num = 0
-    def __init__(self, name, age, grade):
+    def __init__(self, name, age):
         self.name = name
         self.age = age
-        self.grade = grade
         Student.student_num += 1
+    def instroduce(self):
+        return f"我的名字叫{self.name}， 我今年{self.age}岁啦"
+    def change_name(self, new_name):
+        self.name = new_name
     @classmethod
-    def getSchoolName(cls):
+    def get_school_name(cls):
         return cls.school
     @classmethod
     def from_string(cls, infos):
-        name, age, grade = infos.split(',')
-        return cls(name, int(age), grade)
+        name, age = infos.split(',')
+        return cls(name, int(age))
     @classmethod
     def reset_num(cls):
         cls.student_num = 0
-student1 = Student("张三", 7, "一年级")
-student2 = Student("李四", 8, "二年级")
+student1 = Student("张三", 7)
+student2 = Student("李四", 8)
 print(Student.student_num)
-print(student1.name, student1.age, student1.grade)
-print(student2.name, student2.age, student2.grade)
+print(student1.name, student1.age)
+print(student2.name, student2.age)
 student2.school = '吉斯小学'
 print(student1.school)
 print(student2.school)
-print(Student.getSchoolName())
-print(student1.getSchoolName())
-student3 = Student.from_string("王五, 9, 三年级")
-print(student3.name, student3.age, student3.grade)
+print(Student.get_school_name())
+print(student1.get_school_name())
+student3 = Student.from_string("王五, 9")
+print(student3.name, student3.age)
 
 Student.reset_num()
 print(Student.student_num)
+
+print(student1.instroduce())
+print(student2.instroduce())
+
+student1.change_name("张三丰")
+print(student1.instroduce())
--- codes/03/02.py
+++ codes/03/02.py
@@ -13,3 +13,45 @@ student2 = Student("李四", 8, "二年级")
 print(Student.student_num)
 Student.reset_num()
 print(Student.student_num)
+
+class MathUtils:
+    @staticmethod
+    def add(a, b):
+        return a + b
+# 通过类名调用静态方法
+result1 = MathUtils.add(3, 5)
+print(result1)
+
+# 创建类的实例
+math_obj = MathUtils()
+# 通过实例调用静态方法
+result2 = math_obj.add(2, 4)
+print(result2)
+
+
+from datetime import datetime
+
+class DateUtils:
+    @staticmethod
+    def days_between_dates(date1, date2):
+        d1 = datetime.strptime(date1, "%Y-%m-%d")
+        d2 = datetime.strptime(date2, "%Y-%m-%d")
+        return abs((d2 - d1).days)
+
+# 调用静态方法
+date1 = "2023-01-01"
+date2 = "2023-01-10"
+days = DateUtils.days_between_dates(date1, date2)
+print(days)
+
+
+import math
+
+class Shape:
+    @staticmethod
+    def distance(x1, y1, x2, y2):
+        return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+# 调用静态方法
+dist = Shape.distance(1, 2, 4, 6)
+print(dist)
