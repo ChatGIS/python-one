73070653ca7bcf533c26b431fe6fb24b0cfe2ff5
ChatGIS <ChatGIS@outlook.com>
2025-04-29 23:11:27 +0800

    v1.4.5 update oop

--- codes/03/04.py
+++ codes/03/04.py
@@ -1,6 +1,3 @@
-继承性是面向对象编程（OOP）中的一个核心概念，在 Python 里它允许一个类（子类或派生类）继承另一个类（父类、基类或超类）的属性和方法。
-## 1. 继承示例
-```python
 class ParentClass:
     def __init__(self):
         pass
@@ -22,17 +19,8 @@ child = ChildClass()
 child.parent_method()
 # 调用子类的方法
 child.child_method()
-```
-输出内容：
-```text
-这是父类的方法
-这是子类的方法
-```
-在上述代码中，ChildClass 继承自 ParentClass，所以 ChildClass 的实例可以调用 ParentClass 中的 parent_method 方法，
-同时还可以调用自身定义的 child_method 方法。
 
-## 2. 继承性实现代码复用
-```python
+
 class Animal:
     def __init__(self, name, age):
         self.name = name
@@ -62,20 +50,8 @@ dog.sleep()
 # 调用子类特有的方法
 cat.meow()
 dog.bark()
-```
-输出内容：
-````text
-咪咪 正在进食
-旺财 正在睡觉
-咪咪 喵喵叫
-旺财 汪汪叫
-````
-该示例通过继承避免了重复编写代码，提高代码的复用性。
 
-## 3. 多继承
-多继承指的是一个子类能够同时继承多个父类的属性和方法。
-### 3.1. 多继承示例
-```python
+
 class ParentClass1:
     def method1(self):
         print("This is method from ParentClass1")
@@ -96,17 +72,7 @@ child = ChildClass()
 child.method1()
 # 调用父类 2 的方法
 child.method2()
-```
-输出内容：
-```text
-This is method from ParentClass1
-This is method from ParentClass2
-```
-### 3.2. 菱形继承问题
-菱形继承是多继承中常见的问题，当一个子类继承自两个父类，而这两个父类又继承自同一个祖父类时，就会形成菱形结构。
-这可能会导致方法调用的不确定性和代码的复杂性增加。  
-Python 使用方法解析顺序（Method Resolution Order，MRO）来确定在多继承情况下调用方法的顺序。可以通过 __mro__ 属性查看类的 MRO。
-```python
+
 class A:
     def method(self):
         print("A's method")
@@ -129,9 +95,3 @@ class D(B, C):
 print(D.__mro__)
 d = D()
 d.method()
-```
-输出内容：
-```text
-(<class '__main__.D'>, <class '__main__.B'>, <class '__main__.C'>, <class '__main__.A'>, <class 'object'>)
-B's method
-```
