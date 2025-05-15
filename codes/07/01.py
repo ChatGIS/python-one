f6cab82fcbe48036e856ae15d713057d2456a0b9
ChatGIS <ChatGIS@outlook.com>
2025-05-15 22:27:20 +0800

    v1.9.2 import

---
+++ codes/07/01.py
@@ -0,0 +1,11 @@
+import math
+print(math.sqrt(4))  # 输出：2.0
+
+from math import sqrt
+print(sqrt(9))  # 输出：3.0
+
+import math as m
+print(m.sqrt(16))  # 输出：4.0
+
+from math import *
+print(sqrt(25))  # 输出：5.0
